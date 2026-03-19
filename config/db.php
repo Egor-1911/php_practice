dfe97ea67672a3b8a525f5383706215349f726d4
Egor-1911 <egormyrt21@gmail.com>
2026-03-19 18:40:57 +0700

    commit database create

---
+++ config/db.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'jsaasbrx_m4',
+    'username' => 'jsaasbrx',
+    'password' => 'M6HMDa',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => '',
+];
